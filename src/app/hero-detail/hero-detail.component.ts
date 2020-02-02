e07984c8f9bf321ff0e28cb2910248ed93eea712
KSRNTheja <srntkolla@gmail.com>
2020-02-02 16:43:48 +0530

    29-Added 'hero' property as an Input property, as the external HeroesComponent will bind to it.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +7,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  // The hero property must be an Input property, annotated with the @Input() decorator,
+  // because the external HeroesComponent will bind to it
+  @Input()
+  hero: Hero;
 
   constructor() {
   }
